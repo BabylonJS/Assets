5ab686b6c8897e97ffdfc49405c6bfbc9fc29116
Raanan Weber <Raanan.Weber@microsoft.com>
2022-02-23 16:54:18 +0100

    asset lib generator

---
+++ libGenerator/generator.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const path = require("path");
+
+const structure = require("../structure.json");
+
+const baseDirectory = path.resolve(__dirname, "../");
+
+if (!fs.existsSync(`${baseDirectory}/generated`)) {
+  fs.mkdirSync(`${baseDirectory}/generated`);
+}
+
+let declaration = `
+declare namespace Assets {
+    interface Asset {
+        description: string;
+        thumbnail: string;
+        fullPath: string;
+        filename: string;
+        baseDirectory: string;
+    }
+`;
+let source = `
+window.Assets = {`;
+Object.keys(structure).forEach((dir) => {
+  // generate source
+  source += `${dir}: {`;
+  structure[dir].forEach((file) => {
+    source += `
+    "${file.name}": {
+        fullPath: "${file.fullPath}",
+        baseDirectory: "${file.baseDirectory}",
+        description: "${file.description}",
+        thumbnail: "${file.thumbnail}",
+        filename: "${file.filename}"
+    },`;
+  });
+  source += `},`;
+
+  // generate declaration
+  declaration += `
+    const ${dir}: {
+`;
+  structure[dir].forEach((file) => {
+    if (!file.name) {
+      console.log(file);
+    }
+    declaration += `
+    /**
+     * ${file.description}
+     * @thumbnail ${file.thumbnail}
+     */
+    "${file.name}": Asset,`;
+  });
+  declaration += `},`;
+});
+
+source += "};";
+declaration += "}";
+
+fs.writeFileSync(`${baseDirectory}/generated/Assets.js`, source);
+fs.writeFileSync(`${baseDirectory}/generated/Assets.d.ts`, declaration);
---
+++ libGenerator/structureGenerator.js
@@ -0,0 +1,149 @@
+/**
+ * Running this script will update the structure file with newly added files.
+ */
+
+const path = require("path");
+const fs = require("fs");
+const glob = require("glob");
+const imageGenerator = require("image-thumbnail");
+const puppeteer = require("puppeteer");
+
+const generateModelThumbnails = process.env.GENERATE_THUMBNAILS || false;
+
+const getDirectories = (source) =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+
+const thumbnailOptions = {
+  width: 100,
+  height: 100,
+  responseType: "base64",
+  jpegOptions: { force: true, quality: 80 },
+};
+
+const baseDirectory = path.resolve(__dirname, "../");
+
+directoriesToFilter = ["libGenerator", "node_modules", "generated"];
+
+const mainDirectories = getDirectories(baseDirectory).filter(
+  (dir) => !directoriesToFilter.includes(dir) && dir[0] !== "."
+);
+
+const structure = require("../structure.json");
+const fileTypes = {
+  environments: ["jpg", "png", "dds", "env", "hdr"],
+  materials: ["jpg", "png"],
+  meshes: ["babylon", "obj", "stl", "gltf", "glb"],
+  nme: ["json"],
+  particles: ["json"],
+  sound: ["mp3", "ogg", "wav"],
+};
+const process = async () => {
+  // 1. Launch the browser
+  const browser = await puppeteer.launch({
+    headless: process.env.HEADLESS === "true",
+  });
+
+  const page = await browser.newPage();
+  // 2. Open a new page
+  await page.setViewport({ width: 200, height: 200 });
+
+  for (const dir of mainDirectories) {
+    if (!structure[dir] || !Array.isArray(structure[dir])) {
+      structure[dir] = [];
+    }
+    if (fileTypes[dir]) {
+      const files = glob.sync(
+        `${baseDirectory}/${dir}/**/*.+(${fileTypes[dir].join("|")})`
+      );
+      for (const file of files) {
+        // relative path
+        const relativePath = path
+          .relative(baseDirectory, file)
+          .replace(/\\/g, "/");
+        let index = structure[dir].findIndex(
+          (item) => item.fullPath === relativePath
+        );
+        const extension = path.extname(file).replace(".", "");
+        const newAsset = {
+          fullPath: relativePath,
+          baseDirectory: path.dirname(relativePath).replace(/\\/g, "/"),
+          filename: path.basename(file),
+          name: path.basename(file, path.extname(file)),
+          description: `${path.basename(file)} (${dir})`,
+          thumbnail: "",
+        };
+        if (["png", "jpg"].includes(extension)) {
+          try {
+            newAsset.thumbnail =
+              "data:image/jpeg;base64," +
+              (await imageGenerator(
+                path.resolve("../", relativePath),
+                thumbnailOptions
+              ));
+          } catch (err) {
+            console.error(err);
+          }
+        } else if (
+          generateModelThumbnails &&
+          ["babylon", "obj", "stl", "gltf", "glb"].includes(extension)
+        ) {
+          console.log("generating", relativePath);
+
+          // 3. Navigate to URL
+          await page.goto(
+            "https://sandbox.babylonjs.com/?assetUrl=https://assets.babylonjs.com/" +
+              relativePath,
+            {
+              waitUntil: "networkidle0",
+            }
+          );
+          await page.evaluate(
+            (sel) => {
+              const element = document.querySelector("#canvasZone");
+              element.style.height = "100%";
+              var elements = document.querySelectorAll(sel);
+              for (var i = 0; i < elements.length; i++) {
+                elements[i].remove();
+              }
+            },
+            [
+              "#babylonjsLoadingDiv",
+              "#footer",
+              "#scene-explorer-host",
+              "#inspector-host",
+            ]
+          );
+          //   await page.waitForSelector("#babylonjsLoadingDiv", {
+          //     hidden: true,
+          //     timeout: 30000,
+          //   });
+
+          // 4. Take screenshot
+          newAsset.thumbnail =
+            "data:image/jpeg;base64," +
+            (await page.screenshot({
+              encoding: "base64",
+              type: "jpeg",
+              // clip: { x: 200, y: 200, width: 200, height: 200 },
+            }));
+        }
+        if (index !== -1) {
+          structure[dir][index] = {
+            ...structure[dir][index],
+            ...newAsset,
+          };
+        } else {
+          structure[dir].push(newAsset);
+        }
+      }
+    }
+  }
+  await page.close();
+  await browser.close();
+};
+process().then(() => {
+  fs.writeFileSync("../structure.json", JSON.stringify(structure, null, 2));
+});
